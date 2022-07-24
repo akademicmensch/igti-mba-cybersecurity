2859ac2ca67f556e327c347d1a113ba7cd9bdabb
Guilherme da Franca Batista <guilherme.da.franca.batista@gmail.com>
2022-07-24 00:18:24 -0300

    updating

--- project_and_conceptual_model/bff-phishing-campaign-app/campaign-service/app/api/service_gophish.py
+++ project_and_conceptual_model/bff-phishing-campaign-app/campaign-service/app/api/service_gophish.py
@@ -16,3 +16,39 @@ def createGroup(name: string, targets: List(GophishGroup)):
 
 
     return
+
+def sendCampaign():
+    groups = [Group(name='Existing Group')]
+    page = Page(name='Existing Page')
+    template = Template(name='Existing Template')
+    smtp = SMTP(name='Existing Profile')
+    url = 'http://phishing_server'
+    campaign = Campaign(
+        name='Example Campaign', groups=groups, page=page,
+        template=template, smtp=smtp)
+
+    campaign = api.campaigns.post(campaign)
+    print campaign.id
+
+    return
+
+def createTemplate():
+
+    template = Template(name='Test Template',
+    html='<html><body>Click <a href="{{.URL}}">here</a></body></html>')
+
+    template = api.templates.post(template)
+    print template.id
+
+    return
+
+def sendProfile():
+    smtp = SMTP(name='Test SMTP')
+    smtp.host = "localhost:25"
+    smtp.from_address = "John Doe <johndoe@example.com>"
+    smtp.interface_type = "SMTP"
+    smtp.ignore_cert_errors = True
+
+    smtp = api.smtp.post(smtp)
+    print smtp.id
+    return
